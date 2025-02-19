d7cac61ee86c52dbf84ca36192806d932f15bec9
Oksana Paduchak <2992paduhcak80@gmail.com>
2025-02-19 21:57:26 +0200

    homework 10

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,19 +1,22 @@
-import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
-
-import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
+import "../css/1-timer.css"
+
+import flatpickr from "flatpickr";
+import iziToast from "izitoast";
+import iconUrl from "../img/iconError.svg";
 
 const refs = {
   startBtn: document.querySelector('[data-start]'),  
-    dataDays: document.querySelector('[data-days]'),
-    dataHours: document.querySelector('[data-hours]'),
-    dataMins: document.querySelector('[data-minutes]'),
-    dataSecs: document.querySelector('[data-seconds]'),
+  dataDays: document.querySelector('[data-days]'),
+  dataHours: document.querySelector('[data-hours]'),
+  dataMins: document.querySelector('[data-minutes]'),
+  dataSecs: document.querySelector('[data-seconds]'),
   dataInput: document.querySelector('#datetime-picker')
 };
 let userSelectedDate = null;
 let intervalTime = null;
+
 refs.startBtn.disabled = true;
 
 const options = {
@@ -21,52 +24,34 @@ const options = {
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
-   onClose(selectedDates) {
-    const selectedDate = selectedDates[0];
-    if (selectedDate <= new Date()) {
-      iziToast.show({
-          title: 'Erorr',
-          titleColor: '#fff',
-          titleSize: '16px',
-          titleLineHeight: '24px',
-          message: "Please choose a date in the future",
-          messageSize: '16px',
-          messageLineHeight: '24px',
-          messageColor: '#fff',
-          backgroundColor: ' #ef4040',
-          position: 'topRight',          
-                });
-      refs.startBtn.disabled = true;
-    } else {
-      userSelectedDate = selectedDate;
-      refs.startBtn.disabled = false;
-    }
+  onClose(selectedDates) {
+    onCloseDatePicker(selectedDates[0])
   },
 };
 
 flatpickr(refs.dataInput, options);
 
 function start() {
-     refs.startBtn.disabled = true;
-    refs.dataInput.disabled = true;
-    intervalTime = setInterval(() => {
-            tick();
-      }, 1000);
-}
+  refs.startBtn.disabled = true;
+  refs.dataInput.disabled = true;
+  intervalTime = setInterval(() => {
+          tick();
+    }, 1000);
+};
  
 function tick() {
-    const currentTime = new Date();
-    const ms = userSelectedDate - currentTime;
-    const { days, hours, minutes, seconds } = convertMs(ms);
-    timeElement(days, hours, minutes, seconds);
-    if (ms <= 0) {
-        refs.startBtn.disabled = false;
-        refs.dataInput.disabled = true;
-        clearInterval(intervalTime);
-        timeElement(0, 0, 0, 0);
-    }
-    return;    
-}
+  const currentTime = new Date();
+  const ms = userSelectedDate - currentTime;
+  const { days, hours, minutes, seconds } = convertMs(ms);
+  timeElement(days, hours, minutes, seconds);
+  if (ms <= 0) {
+      refs.startBtn.disabled = false;
+      refs.dataInput.disabled = true;
+      clearInterval(intervalTime);
+      timeElement(0, 0, 0, 0);
+  }
+  return;    
+};
 
 refs.startBtn.addEventListener('click', () => {    
   start();
@@ -82,16 +67,37 @@ function convertMs(ms) {
   const minutes = Math.floor(((ms % day) % hour) / minute);
   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
   return { days, hours, minutes, seconds };
-}
+};
 
 function timeToSTR(value) { 
-   return String(value).padStart(2, '0');
-}
+  return String(value).padStart(2, '0');
+};
 
 function timeElement(days, hours, minutes, seconds) {
   refs.dataDays.textContent = timeToSTR(days);
   refs.dataHours.textContent = timeToSTR(hours);
   refs.dataMins.textContent = timeToSTR(minutes);
   refs.dataSecs.textContent = timeToSTR(seconds);
-}
+};
 
+function onCloseDatePicker(selectedDate) {
+  if (selectedDate <= new Date()) {
+    iziToast.show({
+      iconUrl,
+      title: 'Erorr',
+      titleColor: '#fff',
+      titleSize: '16px',
+      titleLineHeight: '24px',
+      message: "Please choose a date in the future",
+      messageSize: '16px',
+      messageLineHeight: '24px',
+      messageColor: '#fff',
+      backgroundColor: ' #ef4040',
+      position: 'topRight',          
+    });
+    refs.startBtn.disabled = true;
+  } else {
+    userSelectedDate = selectedDate;
+    refs.startBtn.disabled = false;
+  }
+};
--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,2 +1,75 @@
 import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
+
+import iconError from "../img/iconError.svg";
+import iconOk from "../img/iconOk.svg";
+
+const form = document.querySelector('form');
+
+form.addEventListener('submit', (evt) => {
+  evt.preventDefault();
+  const delay = Number(evt.target.delay.value);
+  const state = evt.target.state.value;
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (state === "fulfilled") {
+        resolve(delay)
+      }
+
+      if (state === 'rejected') {
+        reject(delay)
+      }
+    }, delay)
+  });
+
+  promise
+    .then((delay) => {
+      const message = ` Fulfilled promise in ${delay}ms`;
+      console.log(message);
+      iziToast.show({
+        message,
+        iconUrl: iconOk,
+        title: 'OK',
+        titleColor: '#fff',
+        titleSize: '16px',
+        titleLineHeight: '24px',
+        messageSize: '16px',
+        messageLineHeight: '24px',
+        messageColor: '#fff',
+        backgroundColor: '#59A10D',
+        position: 'topRight',
+      });
+    });
+    .catch (() => {
+    const message = `❌ Rejected promise in ${delay}ms`;
+    console.log(message);
+    iziToast.show({
+      message,
+      iconUrl: iconError,
+      title: 'Erorr',
+      titleColor: '#fff',
+      titleSize: '16px',
+      titleLineHeight: '24px',
+      messageSize: '16px',
+      messageLineHeight: '24px',
+      messageColor: '#fff',
+      backgroundColor: ' #ef4040',
+      position: 'topRight',
+    });
+  });
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+//*====================================
+
